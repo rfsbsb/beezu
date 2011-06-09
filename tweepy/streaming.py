125a5748940ee5bf1ad16d772fe09c4600c7da47
Rafael Ferreira Silva <rafael@rafaelsilva.net>
2011-06-09 12:00:51 -0300

    Updating tweepy

--- tweepy/streaming.py
+++ tweepy/streaming.py
@@ -71,11 +71,11 @@ class Stream(object):
         self.auth = auth
         self.listener = listener
         self.running = False
-        self.timeout = options.get("timeout") or 5.0
+        self.timeout = options.get("timeout", 300.0)
         self.retry_count = options.get("retry_count")
-        self.retry_time = options.get("retry_time") or 10.0
-        self.snooze_time = options.get("snooze_time") or 5.0
-        self.buffer_size = options.get("buffer_size") or 1500
+        self.retry_time = options.get("retry_time", 10.0)
+        self.snooze_time = options.get("snooze_time",  5.0)
+        self.buffer_size = options.get("buffer_size",  1500)
         if options.get("secure"):
             self.scheme = "https"
         else:
@@ -89,14 +89,13 @@ class Stream(object):
     def _run(self):
         # Authenticate
         url = "%s://%s%s" % (self.scheme, self.host, self.url)
-        self.auth.apply_auth(url, 'POST', self.headers, self.parameters)
 
         # Connect and process the stream
         error_counter = 0
         conn = None
         exception = None
         while self.running:
-            if self.retry_count and error_counter > self.retry_count:
+            if self.retry_count is not None and error_counter > self.retry_count:
                 # quit if error count greater than retry count
                 break
             try:
@@ -104,6 +103,7 @@ class Stream(object):
                     conn = httplib.HTTPConnection(self.host)
                 else:
                     conn = httplib.HTTPSConnection(self.host)
+                self.auth.apply_auth(url, 'POST', self.headers, self.parameters)
                 conn.connect()
                 conn.sock.settimeout(self.timeout)
                 conn.request('POST', self.url, self.body, headers=self.headers)
@@ -133,29 +133,23 @@ class Stream(object):
             conn.close()
 
         if exception:
-            raise exception
+            raise
 
     def _read_loop(self, resp):
-        data = ''
-        while self.running:
+          while self.running:
             if resp.isclosed():
                 break
 
             # read length
-            length = ''
+            data = ''
             while True:
                 c = resp.read(1)
                 if c == '\n':
                     break
-                length += c
-            length = length.strip()
-            if length.isdigit():
-                length = int(length)
-            else:
-                continue
+                data += c
+            data = data.strip()
 
             # read data and pass into listener
-            data = resp.read(length)
             if self.listener.on_data(data) is False:
                 self.running = False
 
@@ -165,6 +159,15 @@ class Stream(object):
             Thread(target=self._run).start()
         else:
             self._run()
+
+    def userstream(self, count=None, async=False, secure=True):
+        if self.running:
+            raise TweepError('Stream object already connected!')
+        self.url = '/2/user.json'
+        self.host='userstream.twitter.com'
+        if count:
+            self.url += '&count=%s' % count
+        self._start(async)
 
     def firehose(self, count=None, async=False):
         self.parameters = {'delimited': 'length'}
@@ -204,8 +207,8 @@ class Stream(object):
         if locations and len(locations) > 0:
             assert len(locations) % 4 == 0
             self.parameters['locations'] = ','.join(['%.2f' % l for l in locations])
-	if count:
-		self.parameters['count'] = count
+        if count:
+            self.parameters['count'] = count
         self.body = urllib.urlencode(self.parameters)
         self.parameters['delimited'] = 'length'
         self._start(async)
